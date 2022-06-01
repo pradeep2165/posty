974790171ededcd19ba491b9bb7461bf200fd609
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-01 23:14:57 +0530

    login controller created, authontication implemented, invalid login handled, error msg showed

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index(){
+        return view("auth.login");
+    }
+    public function store(request $request){
+        //validate
+        $this->validate($request, [
+            'email' => 'required',
+            'password'=>'required'
+        ]);
+        //if user entered invalid credentials
+        if(!auth()->attempt($request->only('email', 'password'))){
+            return back()->with('status', 'Invalid login details');//redirect back to the login page
+        }
+        auth()->attempt($request->only('email', 'password'));
+        return redirect()->route('dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,8 @@ use App\Http\Controllers\DashboardController;
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
 Route::get('/posts', function () {
    return view('posts.index');
 });
