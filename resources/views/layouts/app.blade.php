f28e285f57ceb7efa06d4c3bd6700f4d515ea361
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-01 23:57:25 +0530

    dashboard access given to only logged in user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
 <body class="bg-gray-200">
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
-            <li><a href="" class="p-3">Home</a></li>
-            <li><a href="" class="p-3">Dashboard</a></li>
+            <li><a href="{{route('home')}}" class="p-3">Home</a></li>
+            <li><a href="{{route('dashboard')}}" class="p-3">Dashboard</a></li>
             <li><a href="" class="p-3">Posts</a></li>
         </ul>
         <ul class="flex items-center">
