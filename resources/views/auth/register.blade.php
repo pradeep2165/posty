41b9074f18c677b49c3efc15318b983f255bd539
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-01 19:50:42 +0530

    entered data kept in form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="mb-4">
                     <label for="name" class="sr-only">Name</label>
                     <input type="text" name="name" id="name" placeholder="Your name"
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="">
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="{{old('name')}}">
                     @error('name')
                     <div class="text-red-500 mt-2 text-sm">
                         {{$message}}
@@ -19,7 +19,7 @@
                 <div class="mb-4">
                     <label for="username" class="sr-only">User Name</label>
                     <input type="text" name="username" id="username" placeholder="Your User Name"
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror" value="">
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror" value="{{old('username')}}">
                     @error('username')
                     <div class="text-red-500 mt-2 text-sm">
                         {{$message}}
@@ -29,7 +29,7 @@
                 <div class="mb-4">
                     <label for="email" class="sr-only">Email</label>
                     <input type="text" name="email" id="email" placeholder="Your Email"
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="">
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{old('email')}}">
                     @error('email')
                     <div class="text-red-500 mt-2 text-sm">
                         {{$message}}
