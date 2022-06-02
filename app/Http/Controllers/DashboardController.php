b8d398fe752e57cdc70529339696256c727ba7b0
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-02 23:56:34 +0530

    first mail sent to mailtrap

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Mail;
+use App\Mail\PostLiked;
 class DashboardController extends Controller
 {
     public function __construct(){
         $this->middleware(['auth']);
     }
     public function index(){
-        
+        $user = auth()->user();
+        Mail::to($user)->send(new PostLiked());
         return view("dashboard");
     }
 }
