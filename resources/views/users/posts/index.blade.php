bf09202a2de0a338757a2d79035c017bfd85a1ff
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-02 22:13:25 +0530

    individual post filted when click individual name

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -3,23 +3,7 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-        <form action="{{route('posts')}}" method="post" class="mb-4">
-            @csrf
-            <div class="mb-4">
-                <label for="body" class="sr-only">body</label>
-                <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post something!"></textarea>
-            
-            @error('body')
-            <div class="text-red-500 mt-2 text-sm">
-            {{$message}}
-            </div>
-            @enderror
-        </div>
-        <div>
-            <button class="bg-blue-500 text-white px-4 py-3 rounded font-medium" type="submit">Post</button>
-        </div>
-        </form>
-        @if ($posts->count())
+            @if ($posts->count())
             @foreach ($posts as $post)
                 <div class="mb-4">
                     <a href="{{route('users.posts', $post->user)}}" class="font-bold">{{$post->user->name}} <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span></a>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostLikeController;
+use App\Http\Controllers\UserPostController;
 
 
 /*
@@ -38,6 +39,8 @@ Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.
 
 Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name("posts.like");
 Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy'])->name("posts.like");
+
+Route::get('/users/{user:username}/posts', [UserPostController::class, 'index'])->name("users.posts");
 
 
 
