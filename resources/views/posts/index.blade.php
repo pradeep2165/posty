69044e6c4894d032795600c68bf3056b5a05b82d
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-02 17:58:31 +0530

    query reduced with eagar_loading

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -26,18 +26,22 @@
                     <p class="mb-2">{{$post->body}}</p>
                 </div>
                 <div class="flex items-center">
-                    @if (!$post -> likedBy(auth()->user()))
-                        <form action="{{route('posts.like', $post)}}" method="post" class="mr-1">
-                        @csrf
-                        <button type="submit" class="text-blue-500">Like</button>
-                        </form>
-                    @else
-                        <form action="{{route('posts.like', $post)}}" method="post" class="mr-1">
+                    @auth
+                        
+                    
+                        @if (!$post -> likedBy(auth()->user()))
+                            <form action="{{route('posts.like', $post)}}" method="post" class="mr-1">
                             @csrf
-                            @method('DELETE')
-                            <button type="submit" class="text-blue-500">Unlike</button>
-                        </form>
-                    @endif
+                            <button type="submit" class="text-blue-500">Like</button>
+                            </form>
+                        @else
+                            <form action="{{route('posts.like', $post)}}" method="post" class="mr-1">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-blue-500">Unlike</button>
+                            </form>
+                        @endif
+                    @endauth
                     <span>{{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}</span>
                 </div>
             @endforeach            
