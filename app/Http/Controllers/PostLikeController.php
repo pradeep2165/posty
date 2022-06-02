66c31de8cd1b82455f365a5ac571763d59b57fbc
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-03 01:43:25 +0530

    only for first time like notification send to user per user

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -11,19 +11,23 @@ use App\Models\Post;
 
 class PostLikeController extends Controller
 {
+    
    public function __construct(){
        $this->middleware(['auth']);
    }
 
     public function store(Post $post, Request $request){
+        
         if($post->likedBy($request->user())){
             return response(null, 489);
         }
         $post->likes()->create([
             'user_id' => $request->user()->id,
         ]);
+        if(!$post->likes()->onlyTrashed()->where('user_id', $request->user()->id)->count()){
+            Mail::to($post->user)->send(new PostLiked(auth()->user(), $post));
+        }
         
-        Mail::to($post->user)->send(new PostLiked(auth()->user(), $post));
         return back();
     }
     public function destroy(Post $post, Request $request){
--- app/Models/Like.php
+++ app/Models/Like.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Like extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = [
         'user_id'
-
     ];
 }
