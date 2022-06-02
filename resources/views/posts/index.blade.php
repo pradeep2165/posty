4843f5fa16c034fa23f69612c6c8382ab3b06a5f
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-02 23:01:26 +0530

    user post fetched with individul component

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -21,35 +21,7 @@
         </form>
         @if ($posts->count())
             @foreach ($posts as $post)
-                <div class="mb-4">
-                    <a href="{{route('users.posts', $post->user)}}" class="font-bold">{{$post->user->name}} <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span></a>
-                    <p class="mb-2">{{$post->body}}</p>
-                </div>                   
-                @can('delete', $post)                   
-                    <form action="{{route('posts.destroy', $post)}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button class="text-blue-500" type="submit">Delete</button>
-                    </form>
-                @endcan               
-                <div class="flex items-center">
-                    @auth                      
-                        @if (!$post -> likedBy(auth()->user()))
-                            <form action="{{route('posts.like', $post)}}" method="post" class="mr-1">
-                            @csrf
-                            <button type="submit" class="text-blue-500">Like</button>
-                            </form>
-                        @else
-                            <form action="{{route('posts.like', $post)}}" method="post" class="mr-1">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="text-blue-500">Unlike</button>
-                            </form>
-                        @endif
-
-                    @endauth
-                    <span>{{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}</span>
-                </div>
+               <x-post :post="$post"/>
             @endforeach            
                 {{$posts->links()}}
         @else                
--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -4,41 +4,13 @@
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
             @if ($posts->count())
-            @foreach ($posts as $post)
-                <div class="mb-4">
-                    <a href="{{route('users.posts', $post->user)}}" class="font-bold">{{$post->user->name}} <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span></a>
-                    <p class="mb-2">{{$post->body}}</p>
-                </div>                   
-                @can('delete', $post)                   
-                    <form action="{{route('posts.destroy', $post)}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button class="text-blue-500" type="submit">Delete</button>
-                    </form>
-                @endcan               
-                <div class="flex items-center">
-                    @auth                      
-                        @if (!$post -> likedBy(auth()->user()))
-                            <form action="{{route('posts.like', $post)}}" method="post" class="mr-1">
-                            @csrf
-                            <button type="submit" class="text-blue-500">Like</button>
-                            </form>
-                        @else
-                            <form action="{{route('posts.like', $post)}}" method="post" class="mr-1">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="text-blue-500">Unlike</button>
-                            </form>
-                        @endif
-
-                    @endauth
-                    <span>{{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}</span>
-                </div>
-            @endforeach            
+                @foreach ($posts as $post)
+                    <x-post :post="$post"/>
+                @endforeach            
                 {{$posts->links()}}
-        @else                
-            There are not posts
-        @endif
+             @else                
+                {{$user->name}} does not have any post.
+            @endif
         </div>
     </div>
 @endsection
